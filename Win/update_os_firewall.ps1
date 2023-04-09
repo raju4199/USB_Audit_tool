a868054c88aa1c02f126504c85fb4d34e2f81911
Shrey Kansal <skansal801@gmail.com>
2023-04-09 17:20:02 +0530

    Update update_os_firewall.ps1

--- Win/update_os_firewall.ps1
+++ Win/update_os_firewall.ps1
@@ -1,6 +1,8 @@
+echo @off
 net accounts > C:\Audit\admin_password.txt
 wbadmin get status > C:\Audit\backup_status.txt 
 ipconfig /all > C:\Audit\network_status.txt 
 netstat -ano > C:\Audit\skans\open_ports.txt 
 systeminfo > C:\Audit\skans\os_report.txt 
 systeminfo | findstr /B /C:"OS Name" /C:"OS Version" /C:"Hotfix(s)" > C:\Audit\os_version.txt 
+PAUSE
